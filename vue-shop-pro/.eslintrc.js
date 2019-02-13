7be26263080267a573a251a264f2d974f034a7dc
wll <1625910841@qq.com>
2019-02-13 21:24:30 +0800

    登录页面开发完毕

--- vue-shop-pro/.eslintrc.js
+++ vue-shop-pro/.eslintrc.js
@@ -3,27 +3,27 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   env: {
-    browser: true,
+    browser: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    "plugin:vue/essential",
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    'standard'
+    "standard"
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ["vue"],
   // add your custom rules here
   rules: {
+    // 设置function小括号前边没有空格，也不要报错
+    "space-before-function-paren": ["error", "never"],
     // allow async-await
-    'generator-star-spacing': 'off',
+    // "generator-star-spacing": "off",
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
-}
+};
---
+++ vue-shop-pro/src/components/Login.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="login-container">
+     <div id="login-box">
+        <div id="login-logo">
+           <img src="../assets/logo.png" alt="">
+        </div>
+
+        <el-form :model="loginForm" ref="loginFormRef" :rules="loginRules">
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username">
+              <i slot="prefix" class="iconfont icon-user "></i>
+            </el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input type="password" v-model="loginForm.password">
+               <i slot="prefix" class="iconfont icon-3702mima "></i>
+            </el-input>
+          </el-form-item>
+
+          <el-row>
+            <el-col :offset="15">
+              <el-button type="primary" @click="login">登录</el-button>
+              <el-button type="info" @click="resetForm">重置</el-button>
+            </el-col>
+          </el-row>
+        </el-form>
+     </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      loginRules: {
+        username: [{ required: true, message: '请输入用户名称', trigger: 'blur' }],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        if (valid === true) {
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) {
+            return this.$message.error('用户名或密码不正确')
+          }
+          window.sessionStorage.setItem('token', res.data.token)
+          this.$router.push('./home')
+        }
+      })
+    },
+    resetForm() {
+      this.$refs.loginFormRef.resetFields()
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+#login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  // overflow: hidden;
+  // position: relative;
+  #login-box {
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    #login-logo {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      box-shadow: 0 0 10px #eee;
+      border-radius: 50%;
+      padding: 8px;
+      box-sizing: border-box;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+    .el-form {
+      width: 100%;
+      position: absolute;
+      bottom: 0;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    // i {
+    //   position: absolute;
+    //   bottom: 0;
+    //   left: 5px;
+    // }
+  }
+}
+</style>
